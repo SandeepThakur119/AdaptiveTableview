bb7f6426b35498a3f49d27f4e51436ddd452eca0
Sandeep Thakur <sandeep.thakur01@mphasis.com>
2018-05-20 18:30:48 +0530

    Adding Data Model Class

---
+++ AdaptiveTableview/Model/DataModel.h
@@ -0,0 +1,16 @@
+//
+//  DataModel.h
+//  AdaptiveTableview
+//
+//  Created by Sandeep Thakur on 20/05/18.
+//  Copyright © 2018 Sandeep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataModel : NSObject
+@property(nonatomic,strong) NSString *titleOfRow;
+@property(nonatomic,strong) NSString *imageURL;
+@property(nonatomic,strong)NSString *detailedDescription;
+
+@end
---
+++ AdaptiveTableview/Model/DataModel.m
@@ -0,0 +1,14 @@
+//
+//  DataModel.m
+//  AdaptiveTableview
+//
+//  Created by Sandeep Thakur on 20/05/18.
+//  Copyright © 2018 Sandeep. All rights reserved.
+//
+
+#import "DataModel.h"
+
+@implementation DataModel
+@synthesize detailedDescription,imageURL,titleOfRow;
+
+@end
