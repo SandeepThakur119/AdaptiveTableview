d84434d7058c822b9eb0e5853d7d4e0b3daffe9e
Sandeep Thakur <sandeep.thakur01@mphasis.com>
2018-05-20 13:44:41 +0530

    Adding Project to GitHub

---
+++ AdaptiveTableview/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  AdaptiveTableview
+//
+//  Created by Sandeep Thakur on 20/05/18.
+//  Copyright © 2018 Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ AdaptiveTableview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AdaptiveTableview
+//
+//  Created by Sandeep Thakur on 20/05/18.
+//  Copyright © 2018 Sandeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
